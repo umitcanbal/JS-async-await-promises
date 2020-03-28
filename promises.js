0475a9e0b726dc21464060f19e618aa245764b4d
umitcanbal <umitcanbal@gmail.com>
2020-03-28 21:55:53 +0100

    Promise yarattık, promise resolve olduktan sonra .then() methoduyla getposts fonksiyonunu çağırdık

---
+++ promises.js
@@ -0,0 +1,37 @@
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+function getPosts() {
+  console.log("get post fonksiyonu çağrıldı")
+  setTimeout( () => {
+    let output = "";
+    posts.forEach( (post, index) => {
+      output += `<li>${post.title}</li>`
+    });
+    document.body.innerHTML = output;
+  }, 5000 )
+}
+
+function createPost(post) {
+  return new Promise( (resolve, reject) => {
+    console.log("new promise yaratıldı");
+    setTimeout( () => {
+      posts.push(post);
+
+      const error = false;
+
+      if(!error) {
+        resolve();
+        console.log("resolve fonksiyonu çalıştı");
+      } else {
+        reject("Something went wrong"); 
+      }
+    }, 5000 )
+
+  } )
+}
+
+createPost( { title: "Post Three", body: "This is post three" } )
+  .then( getPosts );
